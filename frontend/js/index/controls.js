83419416d959383f8ea25e70bf359382ffc8df85
Роман Ткачук <tkachukroman64@gmail.com>
2023-01-13 15:36:35 +0200

    Implemented key combinations for movement/camera

--- frontend/js/index/controls.js
+++ frontend/js/index/controls.js
@@ -1,12 +1,17 @@
-document.addEventListener('keyup', (e) => {
-    switch(e.code) {
-        case "KeyW": mgrSocket.send("MOVE W"); break;
-        case "KeyS": mgrSocket.send("MOVE S"); break;
-        case "KeyA": mgrSocket.send("MOVE A"); break;
-        case "KeyD": mgrSocket.send("MOVE D"); break;
-        case "KeyT": offsetUp(); break;
-        case "KeyG": offsetDown(); break;
-        case "KeyF": offsetLeft(); break;
-        case "KeyH": offsetRight(); break;
+document.addEventListener('keydown', (e) => {
+    if (e.shiftKey) {
+        switch(e.code) {
+            case "KeyW": offsetUp(); break;
+            case "KeyS": offsetDown(); break;
+            case "KeyA": offsetLeft(); break;
+            case "KeyD": offsetRight(); break;
+        }
+    } else {
+        switch(e.code) {
+            case "KeyW": mgrSocket.send("MOVE W"); break;
+            case "KeyS": mgrSocket.send("MOVE S"); break;
+            case "KeyA": mgrSocket.send("MOVE A"); break;
+            case "KeyD": mgrSocket.send("MOVE D"); break;
+        }
     }
 })
